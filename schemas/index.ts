863d48079b31078e6757f36c692df1f0cb0ecf05
Jemmima Knight <jemmimaknight@gmail.com>
2023-05-12 14:33:09 +0200

    Remove old schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,13 +1,4 @@
-import author from './documents/author'
-import category from './documents/category'
-import situation from './documents/situation'
-import recommendation from './documents/recommendation'
-import referenceArticle from './documents/referenceArticle'
 
 export const schemaTypes = [
-    situation,
-    recommendation,
-    category,
-    author,
-    referenceArticle
+    
 ]
